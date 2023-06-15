2a22e40e02fab47a4ee9e4ea04cb8d5f5d1024f9
jorelabess-vagrant <jorelabes@gmail.com>
2023-06-15 15:54:24 +0000

    fix correct output error on file 101-

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include<unistd.h>
 
 /**
- *main - the entry point
- *return: Always 1 (Success)
+ *Main - the entry point
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
