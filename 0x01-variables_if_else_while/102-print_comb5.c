cea3babc194c8964a9db0b0d047ed09cbdf828df
jorelabess-vagrant <jorelabes@gmail.com>
2023-06-16 10:58:54 +0000

    fix error by correct use signe on file 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,12 +16,12 @@ int main(void)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
-				putchar(' ');
+				putchar('');
 				putchar((j / 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
-					putchar(' ');
+					putchar('');
 				}
 			}
 		}
